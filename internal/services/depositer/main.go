cf556023b1dd30f62ede429f0071f4279012fd31
zlobste <nkrayniuk@gmail.com>
2021-03-12 18:33:24 +0200

    Parse event logs

--- internal/services/depositer/main.go
+++ internal/services/depositer/main.go
@@ -4,38 +4,40 @@ import (
 	"context"
 	"github.com/GeoDB-Limited/odin-deposit-ether-svc/internal/config"
 	"github.com/GeoDB-Limited/odin-deposit-ether-svc/internal/services/listener"
+	"github.com/GeoDB-Limited/odin-deposit-ether-svc/odin/client"
 	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 type Service struct {
 	config   config.Config
 	log      *logan.Entry
 	listener *listener.Service
+	odin     *client.Client
 }
 
 func New(cfg config.Config) *Service {
 	return &Service{
 		config:   cfg,
 		log:      cfg.Log(),
+		odin:     cfg.OdinClient(),
 		listener: listener.New(cfg),
 	}
 }
 
 func (s *Service) Run(ctx context.Context) (err error) {
-	localCtx, _ := context.WithCancel(ctx)
-	/*
-		defer func() {
-			if rvr := recover(); rvr != nil {
-				// we are spending actual ether here,
-				// so in case of emergency abandon the operations completely
-				cancel()
-				err = errors.Wrap(errors.FromPanic(rvr), "service panicked")
-			}
-		}()
-	*/
+	localCtx, cancel := context.WithCancel(ctx)
 
-	go s.listener.Run(localCtx)
-	s.log.Info("Started listening for deposits")
+	defer func() {
+		if rvr := recover(); rvr != nil {
+			cancel()
+			err = errors.Wrap(errors.FromPanic(rvr), "service panicked")
+		}
+	}()
+
+	if err := s.listener.Run(localCtx); err != nil {
+		err = errors.Wrap(err, "failed to run event listener")
+	}
 
 	return nil
 }
--- internal/services/listener/main.go
+++ internal/services/listener/main.go
@@ -2,20 +2,17 @@ package listener
 
 import (
 	"context"
+	"fmt"
 	"github.com/GeoDB-Limited/odin-deposit-ether-svc/internal/config"
 	"github.com/GeoDB-Limited/odin-deposit-ether-svc/internal/data/system-contracts/generated"
 	"github.com/GeoDB-Limited/odin-deposit-ether-svc/odin/client"
 	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
-	"gitlab.com/distributed_lab/running"
-	"log"
 	"math/big"
-	"strings"
 	"sync"
 	"time"
 )
@@ -24,56 +21,49 @@ type Service struct {
 	log  *logan.Entry
 	eth  *ethclient.Client
 	odin *client.Client
-
-	ch chan TransferDetails
+	ch   chan TransferDetails
 
 	sync.RWMutex
 }
 
-type Transfer struct {
-	From     common.Address
-	To       string
-	Value    *big.Int
-	Decimals int64
-	Raw      types.Log
-}
-
 type TransferDetails struct {
+	UserAddress     common.Address
+	OdinAddress     string
+	DepositAmount   *big.Int
 	TransactionHash string
-	Destination     string
 	BlockTime       time.Time
-	Amount          *big.Int
-	Decimals        int64
 }
 
 func New(cfg config.Config) *Service {
 	ch := make(chan TransferDetails)
 	return &Service{
-		log:  cfg.Log(),
-		eth:  cfg.EtherClient(),
-		odin: cfg.OdinClient(),
-		ch:   ch,
-
+		log:     cfg.Log(),
+		eth:     cfg.EtherClient(),
+		odin:    cfg.OdinClient(),
+		ch:      ch,
 		RWMutex: sync.RWMutex{},
 	}
 }
 
-func (s *Service) Run(ctx context.Context) {
-	go running.WithBackOff(ctx, s.log, "transfer-listener", func(ctx context.Context) error {
-		err := s.subscribe(ctx)
-		if err != nil {
-			return errors.Wrap(err, "failed to subscribe on the contract events")
-		}
-		s.log.Info("Finished listening new transfers")
+func (s *Service) Run(ctx context.Context) error {
+	err := s.subscribe(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to subscribe on the contract events")
+	}
+	s.log.Info("Finished listening new transfers")
 
-		return nil
-	}, time.Minute, time.Minute, time.Hour)
+	return nil
 }
 
 func (s *Service) subscribe(ctx context.Context) error {
 	contractAddress, err := s.odin.GetBridgeAddress()
 	if err != nil {
 		return errors.Wrap(err, "failed to get contract address")
+	}
+
+	contract, err := generated.NewEtherBridge(*contractAddress, s.eth)
+	if err != nil {
+		return errors.Wrap(err, "failed to create a contract instance")
 	}
 
 	query := ethereum.FilterQuery{
@@ -98,7 +88,7 @@ runner:
 				return errors.New("channel closed unexpectedly")
 			}
 
-			err = s.processTransfer(ctx, event)
+			err = s.processTransfer(*contract, event, ctx)
 			if err != nil {
 				return errors.Wrap(err, "failed to process transfer")
 			}
@@ -110,24 +100,16 @@ runner:
 	return nil
 }
 
-func (s *Service) processTransfer(ctx context.Context, event types.Log) error {
+func (s *Service) processTransfer(contract generated.EtherBridge, event types.Log, ctx context.Context) error {
 	if event.Removed {
 		return nil
 	}
 	s.Lock()
 	defer s.Unlock()
 
-	contractAbi, err := abi.JSON(strings.NewReader(generated.BridgeABI))
+	parsed, err := contract.ParseEtherDeposited(event)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	parsed := new(Transfer)
-	if err = contractAbi.UnpackIntoInterface(&parsed, "Transfer", event.Data); err != nil {
-		return errors.Wrap(err, "failed to unpack log", logan.F{
-			"event_data":   event.Data,
-			"event_topics": event.Topics,
-		})
+		return errors.Wrap(err, "failed to parse log")
 	}
 
 	block, err := s.eth.BlockByHash(ctx, event.BlockHash)
@@ -138,13 +120,27 @@ func (s *Service) processTransfer(ctx context.Context, event types.Log) error {
 		})
 	}
 
-	s.ch <- TransferDetails{
+	transferDetails := TransferDetails{
+		UserAddress:     parsed.UserAddress,
+		OdinAddress:     parsed.OdinAddress,
+		DepositAmount:   parsed.DepositAmount,
 		TransactionHash: event.TxHash.String(),
-		Destination:     parsed.To,
-		Amount:          parsed.Value,
 		BlockTime:       time.Unix(int64(block.Time()), 0),
-		Decimals:        parsed.Decimals,
 	}
 
+	s.log.Info(fmt.Sprintf(
+		"%s deposited %s ETH to %s at %s",
+		transferDetails.UserAddress,
+		transferDetails.DepositAmount,
+		transferDetails.OdinAddress,
+		transferDetails.BlockTime.UTC().String(),
+	))
+
+	// s.ch <- transferDetails
+
 	return nil
+}
+
+func (s *Service) GetTransferDetails() <-chan TransferDetails {
+	return s.ch
 }
